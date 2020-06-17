3c4aa040269c16292970b649768019318f8c9e12
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-06-17 12:05:47 -0400

    add executable

---
+++ Integral.Network/Executables/NetworkExecutable.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Integral.Abstractions;
+using Integral.Networks;
+
+namespace Integral.Executables
+{
+    public sealed class NetworkExecutable : Executable<Task>
+    {
+        private readonly Network network;
+
+        private readonly Executable<Task> executable;
+
+        public NetworkExecutable(Network network, Executable<Task> executable)
+        {
+            this.network = network;
+            this.executable = executable;
+        }
+
+        public async Task Execute(CancellationToken cancellationToken)
+        {
+            await network.Initialize(cancellationToken);
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await network.Execute(cancellationToken);
+                await executable.Execute(cancellationToken);
+                await Task.Delay(1);
+            }
+        }
+    }
+}
